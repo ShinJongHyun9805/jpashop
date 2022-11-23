5c20435bda680c3be2e6849cd23f1d0f657b1011
ShinJongHyun9805 <jakie5803@gmail.com>
2022-11-24 07:07:11 +0900

    지연로딩 추가, 연관관계 메서드 생성

--- src/main/java/jpabook2/jpashop2/domain/Order.java
+++ src/main/java/jpabook2/jpashop2/domain/Order.java
@@ -8,6 +8,8 @@ import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
+import static javax.persistence.FetchType.LAZY;
+
 @Entity
 @Table(name = "orders")
 @Getter @Setter
@@ -17,14 +19,18 @@ public class Order {
     @Column(name ="order_id")
     private Long id;
 
-    @ManyToOne // Order의 입장에서 Member는 다대일 관계(여러개의 주문을 한 하나의 고객)
+    // Order의 입장에서 Member는 다대일 관계(여러개의 주문을 한 하나의 고객)
+    // FetchType EAGER을 사용하게 되면 N + 1 즉 Select 쿼리문 100개가 나간다고함.
+    // 무조건 지연로딩 LAZY를 사용해야함.
+    @ManyToOne(fetch = LAZY)
     @JoinColumn(name = "member_id") // FK가 member_id가 된다.
     private Member member;
 
-    @OneToMany(mappedBy = "order")
-    private List<OrderItem> orderItems = new ArrayList<>();
+    @OneToMany(mappedBy = "order", cascade = CascadeType.ALL)
+    private List<OrderItem> orderItems = new ArrayList<>(); // 필드에서 초기화하는게 최고
 
-    @OneToOne
+    // *ToONe은 다 지연로딩 해야함.
+    @OneToOne(fetch = LAZY, cascade = CascadeType.ALL)
     @JoinColumn(name = "delivery_id")
     private Delivery delivery;
 
@@ -32,4 +38,22 @@ public class Order {
 
     @Enumerated(EnumType.STRING)
     private OrderStatus status;         // 주문상태 [ORDER, CANCEL]
+
+    /**
+     * 연관관계 메서드
+     **/
+    public void setMember(Member member){
+        this.member = member;
+        member.getOrders().add(this);
+    }
+
+    public void addOrderItem(OrderItem orderItem){
+       orderItems.add(orderItem);
+       orderItem.setOrder(this);
+    }
+
+    public void setDelivery(Delivery delivery){
+        this.delivery = delivery;
+        delivery.setOrder(this);
+    }
 }
