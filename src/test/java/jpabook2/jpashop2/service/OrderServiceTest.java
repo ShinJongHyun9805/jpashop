c204905807067818a69e955bb87949c2a4844cb4
ShinJongHyun9805 <jakie5803@gmail.com>
2022-12-12 07:07:39 +0900

    주문 도메인 개발_2

---
+++ src/test/java/jpabook2/jpashop2/service/OrderServiceTest.java
@@ -0,0 +1,16 @@
+package jpabook2.jpashop2.service;
+
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest
+@Transactional
+class OrderServiceTest {
+
+
+}
