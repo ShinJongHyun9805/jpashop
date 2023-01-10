845a69de76531486be3ddf052e7d55573609c377
ShinJongHyun9805 <jakie5803@gmail.com>
2023-01-10 23:56:24 +0900

    회원가입 페이지 Valid 사용하기위해 valid 디펜던시 추가 회원가입 시 필수값 valid, @NotEmpty

---
+++ src/main/java/jpabook2/jpashop2/controller/MemberController.java
@@ -0,0 +1,49 @@
+package jpabook2.jpashop2.controller;
+
+import jpabook2.jpashop2.domain.Address;
+import jpabook2.jpashop2.domain.Member;
+import jpabook2.jpashop2.service.MemberService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import javax.validation.Valid;
+
+@Controller
+@RequiredArgsConstructor
+public class MemberController {
+
+    private final MemberService memberService;
+
+    @GetMapping("/members/new")
+    public String createForm(Model model){
+        model.addAttribute("memberForm", new MemberForm());
+
+        return "members/createMemberForm";
+    }
+
+    @PostMapping("/members/new")
+    public String create(@Valid MemberForm form, BindingResult result){
+        // @Valid class 파일에 @NotEmpty 같은 밸리데이션 기능을 사용하겠다 선언
+        // form class팔엘 @NotEmpty로 걸리면 에러페이지 발생하는데 BindingResult쓰면
+        // 에러발생시 원하는 페이지로 return 가능함.
+        // 그럼 원하는 return이 가능하면, 중개 if 절도 고칠수있으려나?
+
+        if (result.hasErrors()){
+            return "members/createMemberForm";
+        }
+
+        Address address = new Address(form.getCity(), form.getStreet(), form.getZipcode());
+
+        Member member = new Member();
+        member.setName(form.getName());
+        member.setAddress(address);
+
+        memberService.join(member);
+
+        return "redirect:/";
+    }
+}
